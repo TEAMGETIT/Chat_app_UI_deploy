a6156bbfccbf492413d812e8e6ea251f55f41ca8
Santhosh1203122 <rm.santhosh1234@gmail.com>
2019-07-19 18:28:45 +0530

    Initial Commit

---
+++ precache-manifest.2740037fb09bf8fd3806c006ca033fee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924901b14cab26b75a43ea61b0e72695",
+    "url": "/Chat_App_UI/index.html"
+  },
+  {
+    "revision": "f1fbb0bb6d066b6a2063",
+    "url": "/Chat_App_UI/static/css/main.b808d5bd.chunk.css"
+  },
+  {
+    "revision": "9813199760ef88033c60",
+    "url": "/Chat_App_UI/static/js/2.f9632810.chunk.js"
+  },
+  {
+    "revision": "f1fbb0bb6d066b6a2063",
+    "url": "/Chat_App_UI/static/js/main.6ec80f62.chunk.js"
+  },
+  {
+    "revision": "982f7d04b428a757793d",
+    "url": "/Chat_App_UI/static/js/runtime~main.59aa22e7.js"
+  },
+  {
+    "revision": "34d35a0da8986ffdf6fde6f15182bbfe",
+    "url": "/Chat_App_UI/static/media/bg.34d35a0d.jpg"
+  }
+]);
